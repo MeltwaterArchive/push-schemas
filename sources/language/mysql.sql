e3994e085722505843f450e83736ff284727baa7
quipo <l.alberton@quipo.it>
2013-12-05 18:05:08 +0000

    language

---
+++ sources/language/mysql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS language (
+  interaction_id VARCHAR(64) PRIMARY KEY NOT NULL,
+  interaction_type VARCHAR(64) NOT NULL,
+  created_at DATETIME DEFAULT NULL,
+  language VARCHAR(5) DEFAULT NULL,
+  confidence DECIMAL DEFAULT NULL
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+CREATE INDEX language_interaction_id_idx ON language (interaction_id);
+CREATE INDEX language_interaction_type_idx ON language (interaction_type);
+CREATE INDEX language_created_at_idx ON language (created_at);
---
+++ sources/language/postgresql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS language (
+  interaction_id VARCHAR(64) PRIMARY KEY NOT NULL,
+  interaction_type VARCHAR(64) NOT NULL,
+  created_at TIMESTAMP DEFAULT NULL,
+  language VARCHAR(5) DEFAULT NULL,
+  confidence DECIMAL DEFAULT NULL
+);
+CREATE INDEX language_interaction_id_idx ON language (interaction_id);
+CREATE INDEX language_interaction_type_idx ON language (interaction_type);
+CREATE INDEX language_created_at_idx ON language (created_at);
