ddf55ec17da5db3a39be94dc895bca96e8026848
Ollie Parsley <ollie@ollieparsley.com>
2013-12-10 15:49:48 +0000

    Updated demographics to include interaction_type and correct indexes

--- augmentations/demographic/mysql.sql
+++ augmentations/demographic/mysql.sql
@@ -1,5 +1,6 @@
 CREATE TABLE demographic (
   interaction_id VARCHAR(64) PRIMARY KEY,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   age_range_start VARCHAR(255) DEFAULT NULL,
   age_range_end VARCHAR(255) DEFAULT NULL,
@@ -17,6 +18,8 @@ CREATE TABLE demographic (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
+CREATE INDEX demographic_interaction_id_idx ON demographic (interaction_id);
+CREATE INDEX demographic_interaction_id_idx ON demographic (interaction_type);
 CREATE INDEX demographic_created_at_idx ON demographic (created_at);
 
 
@@ -29,11 +32,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_likes_and_interests_interaction_id_idx ON demographic_likes_and_interests (interaction_id);
+CREATE INDEX demographic_likes_and_interests_interaction_type_idx ON demographic_likes_and_interests (interaction_type);
 CREATE INDEX demographic_likes_and_interests_created_at_idx ON demographic_likes_and_interests (created_at);
 
 
 CREATE TABLE demographic_twitter_accounts_categories (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   category VARCHAR(255) DEFAULT NULL
 )
@@ -41,11 +46,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_twitter_accounts_categories_interaction_id_idx ON demographic_twitter_accounts_categories (interaction_id);
+CREATE INDEX demographic_twitter_accounts_categories_interaction_type_idx ON demographic_twitter_accounts_categories (interaction_type);
 CREATE INDEX demographic_twitter_accounts_categories_created_at_idx ON demographic_twitter_accounts_categories (created_at);
 
 
 CREATE TABLE demographic_twitter_accounts_large (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   account VARCHAR(255) DEFAULT NULL
 )
@@ -53,11 +60,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_twitter_accounts_large_interaction_id_idx ON demographic_twitter_accounts_large (interaction_id);
+CREATE INDEX demographic_twitter_accounts_large_interaction_type_idx ON demographic_twitter_accounts_large (interaction_type);
 CREATE INDEX demographic_twitter_accounts_large_created_at_idx ON demographic_twitter_accounts_large (created_at);
 
 
 CREATE TABLE demographic_dressed_by (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   brand VARCHAR(255) DEFAULT NULL
 )
@@ -65,11 +74,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_dressed_by_interaction_id_idx ON demographic_dressed_by (interaction_id);
+CREATE INDEX demographic_dressed_by_interaction_type_idx ON demographic_dressed_by (interaction_type);
 CREATE INDEX demographic_dressed_by_created_at_idx ON demographic_dressed_by (created_at);
 
 
 CREATE TABLE demographic_eat_drink_at (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   place VARCHAR(255) DEFAULT NULL
 )
@@ -77,11 +88,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_eat_drink_at_interaction_id_idx ON demographic_eat_drink_at (interaction_id);
+CREATE INDEX demographic_eat_drink_at_interaction_type_idx ON demographic_eat_drink_at (interaction_type);
 CREATE INDEX demographic_eat_drink_at_created_at_idx ON demographic_eat_drink_at (created_at);
 
 
 CREATE TABLE demographic_shop_at (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   shop VARCHAR(255) DEFAULT NULL
 )
@@ -89,11 +102,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_shop_at_interaction_id_idx ON demographic_shop_at (interaction_id);
+CREATE INDEX demographic_shop_at_interaction_type_idx ON demographic_shop_at (interaction_type);
 CREATE INDEX demographic_shop_at_created_at_idx ON demographic_shop_at (created_at);
 
 
 CREATE TABLE demographic_professions (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   profession VARCHAR(255) DEFAULT NULL
 )
@@ -101,11 +116,13 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_professions_interaction_id_idx ON demographic_professions (interaction_id);
+CREATE INDEX demographic_professions_interaction_type_idx ON demographic_professions (interaction_type);
 CREATE INDEX demographic_professions_created_at_idx ON demographic_professions (created_at);
 
 
 CREATE TABLE demographic_services (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at DATETIME NOT NULL,
   service VARCHAR(255) DEFAULT NULL
 )
@@ -113,4 +130,5 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 CREATE INDEX demographic_services_interaction_id_idx ON demographic_services (interaction_id);
+CREATE INDEX demographic_services_interaction_type_idx ON demographic_services (interaction_type);
 CREATE INDEX demographic_services_created_at_idx ON demographic_services (created_at);
--- augmentations/demographic/postgresql.sql
+++ augmentations/demographic/postgresql.sql
@@ -1,5 +1,6 @@
 CREATE TABLE demographic (
   interaction_id VARCHAR(64) PRIMARY KEY,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   age_range_start VARCHAR(255) DEFAULT NULL,
   age_range_end VARCHAR(255) DEFAULT NULL,
@@ -14,76 +15,94 @@ CREATE TABLE demographic (
   twitter_accounts_categories VARCHAR(255) DEFAULT NULL,
   twitter_activity VARCHAR(255) DEFAULT NULL
 );
+CREATE INDEX demographic_interaction_id_idx ON demographic (interaction_id);
+CREATE INDEX demographic_interaction_type_idx ON demographic (interaction_type);
 CREATE INDEX demographic_created_at_idx ON demographic (created_at);
 
 
 CREATE TABLE demographic_likes_and_interests (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   interest VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX demographic_likes_and_interests_interaction_id_idx ON demographic_likes_and_interests (interaction_id);
+CREATE INDEX demographic_likes_and_interests_interaction_type_idx ON demographic_likes_and_interests (interaction_type);
 CREATE INDEX demographic_likes_and_interests_created_at_idx ON demographic_likes_and_interests (created_at);
 
 
 CREATE TABLE demographic_twitter_accounts_categories (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   category VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX demographic_twitter_accounts_categories_interaction_id_idx ON demographic_twitter_accounts_categories (interaction_id);
+CREATE INDEX demographic_twitter_accounts_categories_interaction_type_idx ON demographic_twitter_accounts_categories (interaction_type);
 CREATE INDEX demographic_twitter_accounts_categories_created_at_idx ON demographic_twitter_accounts_categories (created_at);
 
 
 CREATE TABLE demographic_twitter_accounts_large (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   account VARCHAR(255) DEFAULT NULL
 );
+CREATE INDEX demographic_twitter_accounts_large_interaction_id_idx ON demographic_twitter_accounts_large (interaction_id);
 CREATE INDEX demographic_twitter_accounts_large_interaction_id_idx ON demographic_twitter_accounts_large (interaction_id);
 CREATE INDEX demographic_twitter_accounts_large_created_at_idx ON demographic_twitter_accounts_large (created_at);
 
 
 CREATE TABLE demographic_dressed_by (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   brand VARCHAR(255) DEFAULT NULL
 );
+CREATE INDEX demographic_dressed_by_interaction_id_idx ON demographic_dressed_by (interaction_id);
 CREATE INDEX demographic_dressed_by_interaction_id_idx ON demographic_dressed_by (interaction_id);
 CREATE INDEX demographic_dressed_by_created_at_idx ON demographic_dressed_by (created_at);
 
 
 CREATE TABLE demographic_eat_drink_at (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   place VARCHAR(255) DEFAULT NULL
 );
+CREATE INDEX demographic_eat_drink_at_interaction_id_idx ON demographic_eat_drink_at (interaction_id);
 CREATE INDEX demographic_eat_drink_at_interaction_id_idx ON demographic_eat_drink_at (interaction_id);
 CREATE INDEX demographic_eat_drink_at_created_at_idx ON demographic_eat_drink_at (created_at);
 
 
 CREATE TABLE demographic_shop_at (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   shop VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX demographic_shop_at_interaction_id_idx ON demographic_shop_at (interaction_id);
+CREATE INDEX demographic_shop_at_interaction_type_idx ON demographic_shop_at (interaction_type);
 CREATE INDEX demographic_shop_at_created_at_idx ON demographic_shop_at (created_at);
 
 
 CREATE TABLE demographic_professions (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   profession VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX demographic_professions_interaction_id_idx ON demographic_professions (interaction_id);
+CREATE INDEX demographic_professions_interaction_type_idx ON demographic_professions (interaction_type);
 CREATE INDEX demographic_professions_created_at_idx ON demographic_professions (created_at);
 
 
 CREATE TABLE demographic_services (
   interaction_id VARCHAR(64) NOT NULL,
+  interaction_type VARCHAR(50) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   service VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX demographic_services_interaction_id_idx ON demographic_services (interaction_id);
+CREATE INDEX demographic_services_interaction_type_idx ON demographic_services (interaction_type);
 CREATE INDEX demographic_services_created_at_idx ON demographic_services (created_at);
