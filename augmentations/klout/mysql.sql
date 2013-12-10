09ab4212552fa0f844001d70e0a170cc792d4a20
Ollie Parsley <ollie@ollieparsley.com>
2013-12-10 14:20:50 +0000

    Made klout topic indexable

--- augmentations/klout/mysql.sql
+++ augmentations/klout/mysql.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS klout_topics (
   interaction_id VARCHAR(64) NOT NULL,
   interaction_type VARCHAR(64) NOT NULL,
   created_at DATETIME DEFAULT NULL,
-  topic TEXT DEFAULT NULL
+  topic VARCHAR(255) DEFAULT NULL
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
@@ -28,3 +28,4 @@ COLLATE = utf8_general_ci;
 CREATE INDEX klout_topics_interaction_id_idx ON klout_topics (interaction_id);
 CREATE INDEX klout_topics_interaction_type_idx ON klout_topics (interaction_type);
 CREATE INDEX klout_topics_created_at_idx ON klout_topics (created_at);
+CREATE INDEX klout_topics_topic_idx ON klout_topics (topic);
--- augmentations/klout/postgresql.sql
+++ augmentations/klout/postgresql.sql
@@ -17,8 +17,9 @@ CREATE TABLE IF NOT EXISTS klout_topics (
   interaction_id VARCHAR(64) NOT NULL,
   interaction_type VARCHAR(64) NOT NULL,
   created_at TIMESTAMP DEFAULT NULL,
-  topic TEXT DEFAULT NULL
+  topic VARCHAR(255) DEFAULT NULL
 );
 CREATE INDEX klout_topics_interaction_id_idx ON klout_topics (interaction_id);
 CREATE INDEX klout_topics_interaction_type_idx ON klout_topics (interaction_type);
 CREATE INDEX klout_topics_created_at_idx ON klout_topics (created_at);
+CREATE INDEX klout_topics_topic_idx ON klout_topics (topic);
