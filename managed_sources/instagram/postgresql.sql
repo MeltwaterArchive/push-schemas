454cd8bbd760f1cae0bf0199d2a3547367667753
Ollie Parsley <ollie@ollieparsley.com>
2013-12-11 10:42:44 +0000

    Fixed twitter and instagram geo data types

--- managed_sources/instagram/postgresql.sql
+++ managed_sources/instagram/postgresql.sql
@@ -1,52 +1,77 @@
-CREATE TABLE IF NOT EXISTS wordpress (
+CREATE TABLE IF NOT EXISTS instagram (
   interaction_id VARCHAR(64) PRIMARY KEY,
   created_at TIMESTAMP DEFAULT NULL,
   type VARCHAR(50) DEFAULT NULL,
-  title VARCHAR(255) DEFAULT NULL,
-  content TEXT DEFAULT NULL,
-  summary TEXT DEFAULT NULL,
-  permalink VARCHAR(255) DEFAULT NULL,
-  updated_at TIMESTAMP DEFAULT NULL,
-  published_at TIMESTAMP DEFAULT NULL,
   link VARCHAR(255) DEFAULT NULL,
-  in_reply_to VARCHAR(255) DEFAULT NULL,
-  comment_id VARCHAR(255) DEFAULT NULL  
+  caption TEXT DEFAULT NULL,
+  location_id VARCHAR(64) DEFAULT NULL,
+  location_name VARCHAR(64) DEFAULT NULL,
+  latitude DECIMAL DEFAULT NULL,
+  longitude DECIMAL DEFAULT NULL
 );
-CREATE INDEX wordpress_interaction_id_idx ON wordpress (interaction_id);
-CREATE INDEX wordpress_created_at_idx ON wordpress (created_at);
+CREATE INDEX instagram_interaction_id_idx ON instagram (interaction_id);
+CREATE INDEX instagram_created_at_idx ON instagram (created_at);
 
-CREATE TABLE IF NOT EXISTS wordpress_author (
+CREATE TABLE IF NOT EXISTS instagram_tags (
+  interaction_id VARCHAR(64) NOT NULL,
+  created_at TIMESTAMP DEFAULT NULL,
+  tag VARCHAR(50) DEFAULT NULL
+);
+CREATE INDEX instagram_tags_interaction_id_idx ON instagram_tags (interaction_id);
+CREATE INDEX instagram_tags_created_at_idx ON instagram_tags (created_at);
+CREATE INDEX instagram_tags_tag_idx ON instagram_tags (tag);
+
+CREATE TABLE IF NOT EXISTS instagram_from (
   interaction_id VARCHAR(64) NOT NULL,
   created_at TIMESTAMP DEFAULT NULL,
   id VARCHAR(255) DEFAULT NULL,
-  username VARCHAR(255) DEFAULT NULL,
-  link VARCHAR(255) DEFAULT NULL
+  username VARCHAR(50) DEFAULT NULL,
+  full_name VARCHAR(50) DEFAULT NULL,
+  bio VARCHAR(255) DEFAULT NULL,
+  website VARCHAR(255) DEFAULT NULL,
+  profile_picture VARCHAR(255) DEFAULT NULL
 );
-CREATE INDEX wordpress_author_interaction_id_idx ON wordpress_author (interaction_id);
-CREATE INDEX wordpress_author_created_at_idx ON wordpress_author (created_at);
+CREATE INDEX instagram_from_interaction_id_idx ON instagram_from (interaction_id);
+CREATE INDEX instagram_from_created_at_idx ON instagram_from (created_at);
 
-CREATE TABLE IF NOT EXISTS wordpress_blog (
+CREATE TABLE IF NOT EXISTS instagram_images (
   interaction_id VARCHAR(64) NOT NULL,
   created_at TIMESTAMP DEFAULT NULL,
-  name VARCHAR(255) DEFAULT NULL,
-  summary TEXT DEFAULT NULL,
-  link VARCHAR(255) DEFAULT NULL,
-  domain VARCHAR(255) DEFAULT NULL,
-  feed VARCHAR(255) DEFAULT NULL,
-  lang VARCHAR(50) DEFAULT NULL,
-  id VARCHAR(255) DEFAULT NULL,
-  blog_id VARCHAR(255) DEFAULT NULL
+  low_resolution_url VARCHAR(255) DEFAULT NULL,
+  low_resolution_width INTEGER DEFAULT NULL,
+  low_resolution_height INTEGER DEFAULT NULL,
+  thumbnail_resolution_url VARCHAR(255) DEFAULT NULL,
+  thumbnail_resolution_width INTEGER DEFAULT NULL,
+  thumbnail_resolution_height INTEGER DEFAULT NULL,
+  standard_resolution_url VARCHAR(255) DEFAULT NULL,
+  standard_resolution_width INTEGER DEFAULT NULL,
+  standard_resolution_height INTEGER DEFAULT NULL
 );
-CREATE INDEX wordpress_blog_interaction_id_idx ON wordpress_blog (interaction_id);
-CREATE INDEX wordpress_blog_created_at_idx ON wordpress_blog (created_at);
+CREATE INDEX instagram_images_interaction_id_idx ON instagram_images (interaction_id);
+CREATE INDEX instagram_images_created_at_idx ON instagram_images (created_at);
 
-CREATE TABLE IF NOT EXISTS wordpress_article (
+CREATE TABLE IF NOT EXISTS instagram_media (
   interaction_id VARCHAR(64) NOT NULL,
   created_at TIMESTAMP DEFAULT NULL,
-  title VARCHAR(255) DEFAULT NULL,
+  id VARCHAR(64) DEFAULT NULL,
+  type VARCHAR(50) DEFAULT NULL,
+  username VARCHAR(50) DEFAULT NULL,
   link VARCHAR(255) DEFAULT NULL,
-  blog_id VARCHAR(255) DEFAULT NULL,
-  post_id VARCHAR(255) DEFAULT NULL
+  caption TEXT DEFAULT NULL,
+  filter VARCHAR(50) DEFAULT NULL,
+  image VARCHAR(255) DEFAULT NULL,
+  video VARCHAR(255) DEFAULT NULL
 );
-CREATE INDEX wordpress_article_interaction_id_idx ON wordpress_article (interaction_id);
-CREATE INDEX wordpress_article_created_at_idx ON wordpress_article (created_at);
+CREATE INDEX instagram_media_interaction_id_idx ON instagram_media (interaction_id);
+CREATE INDEX instagram_media_created_at_idx ON instagram_media (created_at);
+
+CREATE TABLE IF NOT EXISTS instagram_media_tags (
+  interaction_id VARCHAR(64) NOT NULL,
+  created_at TIMESTAMP DEFAULT NULL,
+  tag VARCHAR(50) DEFAULT NULL
+);
+CREATE INDEX instagram_media_tags_interaction_id_idx ON instagram_media_tags (interaction_id);
+CREATE INDEX instagram_media_tags_created_at_idx ON instagram_media_tags (created_at);
+CREATE INDEX instagram_media_tags_tag_idx ON instagram_media_tags (tag);
+
+
